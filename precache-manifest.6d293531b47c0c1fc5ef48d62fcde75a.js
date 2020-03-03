688ac25e38994e0bc7974da9082913e6e3122625
Basavaraj_300068368 <basavaraj.hadimani@myntra.com>
2020-03-03 22:01:39 +0530

    Updates

---
+++ precache-manifest.6d293531b47c0c1fc5ef48d62fcde75a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a212614d82fc1627133492be4d3294b1",
+    "url": "/PWA/index.html"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/PWA/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9761b8c298af74748e16",
+    "url": "/PWA/static/css/main.c014e362.chunk.css"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/PWA/static/js/2.a5747d58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/PWA/static/js/2.a5747d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9761b8c298af74748e16",
+    "url": "/PWA/static/js/main.ee0dd2e9.chunk.js"
+  },
+  {
+    "revision": "f4c6fd81158ee89bd7cc",
+    "url": "/PWA/static/js/runtime-main.9e674068.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/PWA/static/media/SEGA.12a3db8b.woff"
+  }
+]);
